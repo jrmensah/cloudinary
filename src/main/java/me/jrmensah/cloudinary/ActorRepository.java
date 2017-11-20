991c2203bfff1e724033284708a09eedd9197842
jrmensah <jerilynmensah@gmail.com>
2017-11-20 11:01:23 -0500

    Cloudinary example

---
+++ src/main/java/me/jrmensah/cloudinary/ActorRepository.java
@@ -0,0 +1,6 @@
+package me.jrmensah.cloudinary;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ActorRepository extends CrudRepository<Actor, Long> {
+}
---
+++ src/main/java/me/jrmensah/cloudinary/CloudinaryApplication.java
@@ -0,0 +1,12 @@
+package me.jrmensah.cloudinary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudinaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudinaryApplication.class, args);
+	}
+}
